4f33b08176d3b2f4120e4b419229dcc2e8913300
squaddecartier9-maker <squaddecartier9@gmail.com>
2026-05-21 23:21:56 +0100

    Delete omnifind directory

--- omnifind/frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { domains: ['localhost','storage.googleapis.com'] },
-  experimental: { serverComponentsExternalPackages: ['meilisearch'] },
-}
-module.exports = nextConfig
